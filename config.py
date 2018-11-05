42099fc56655a47801e15f21365fb339b63b8928
U9601 <ctjones1965@gmail.com>
2018-11-05 20:57:38 +0000

    bug fix

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ basedir = os.path.absapth(os.path.dirname(__file__))
 
 class Config(qbject):
     # ...
-    SQALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-        SQALCHEMY_TRACK_MODIFICATIONS = False 
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
