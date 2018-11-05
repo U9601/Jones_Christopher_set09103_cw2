622725d6794c6d09125b2f5df48734cd780d6477
U9601 <ctjones1965@gmail.com>
2018-11-05 14:38:06 +0000

    adding a favicon

--- home.py
+++ home.py
@@ -1,2 +1,10 @@
 from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    name = ''
+    return render_template('home.html', name = name)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
