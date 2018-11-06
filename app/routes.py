eef3abae3c0cf7e7375a4d85b4798907bee52b03
U9601 <ctjones1965@gmail.com>
2018-11-06 13:24:29 +0000

    bug fix

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template, url_for, redirect
 from app import app
+from templates import templates
 
 @app.route('/')
 def home():
     name = ''
-    return render_template('templates/home.html', name = name)
+    return render_template('/home.html', name = name)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
