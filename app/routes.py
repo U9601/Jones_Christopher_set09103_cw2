2d06ba9733992645ccbe5126c930a54c08c7df5a
U9601 <ctjones1965@gmail.com>
2018-11-07 13:35:29 +0000

    added new templates

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,12 @@ from app import app
 @app.route('/')
 def home():
     name = ''
-    return render_template('home.html', name = name)
+    return render_template('news.html', name = name)
+
+@app.route('/matches')
+def mathes():
+    name = ''
+    return render_template('')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
