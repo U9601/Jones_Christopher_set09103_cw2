caa4a1407088387055ff73f7e6260a21ecc9dd09
U9601 <ctjones1965@gmail.com>
2018-11-07 13:48:50 +0000

    adding in routes

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,22 @@ def home():
 @app.route('/matches')
 def mathes():
     name = ''
-    return render_template('')
+    return render_template('matches.html', name = name)
+
+@app.route('/login')
+def mathes():
+    name = ''
+    return render_template('login.html', name = name)
+
+@app.route('/fourm')
+def mathes():
+    name = ''
+    return render_template('fourm.html', name = name)
+
+@app.route('/results')
+def mathes():
+    name = ''
+    return render_template('results.html', name = name)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
