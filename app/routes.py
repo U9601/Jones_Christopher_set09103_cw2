f557abb4b819673f4491bf97e706e24e94f6df13
U9601 <ctjones1965@gmail.com>
2018-11-07 16:29:14 +0000

    adding log in system

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
 from flask import render_template, url_for, redirect
+from flask_login import current_user, login_user, login_required
+from app.models import User
 from app import app
 
 @app.route('/')
@@ -11,12 +13,30 @@ def matches():
     name = ''
     return render_template('matches.html', name = name)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     name = ''
-    return render_template('login.html', name = name)
+    if(current_user.is_authenticated:
+        return redirect(url_for(''))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('dashbaord')
+        return redirect(next_page)
+    return render_template('login.html', title = 'Sign In', form = form name = name)
 
-@app.route('/fourm')
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for(''))
+
+@app.route('/forum')
 def fourm():
     name = ''
     return render_template('fourm.html', name = name)
@@ -25,6 +45,29 @@ def fourm():
 def results():
     name = ''
     return render_template('results.html', name = name)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    name = ''
+    return render_template('dashboard.html', posts=posts, name = name)
+
+@app.route('/register')
+def register():
+    name = ''
+    if current_user.is_authenticated:
+        return redirect(url_for('dashbaord'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You are now a user Pog')
+        return redirect(url_for('')
+    return render_template('register.html', name = name, form = form)
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
