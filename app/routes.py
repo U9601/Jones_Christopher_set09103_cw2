0ad81fd9d9936f64c00f0dd66a7024517ea5cde3
U9601 <ctjones1965@gmail.com>
2018-11-12 16:52:58 +0000

    bug fix

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,17 @@ from werkzeug.urls import url_parse
 
 @app.route('/')
 def home():
-    return render_template('news.html')
+    name = ''
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+    return render_template('news.html' form = form, name = name)
 
 @app.route('/matches')
 def matches():
