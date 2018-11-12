080b96563fb81b51ae1d8e928a0d83465a53bb9a
U9601 <ctjones1965@gmail.com>
2018-11-12 17:19:26 +0000

    bug fix

--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,6 @@ from werkzeug.urls import url_parse
 @app.route('/news', methods=['GET', 'POST'])
 def news():
     name = ''
-    if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -37,10 +35,6 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('dashboard')
-        return redirect(next_page)
     return render_template('login.html', title = 'Sign In', form = form, name = name)
 
 @app.route('/logout')
