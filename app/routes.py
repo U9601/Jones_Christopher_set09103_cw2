3b60ab19ee56893f176c03dbf547998cc7af2da7
U9601 <ctjones1965@gmail.com>
2018-11-12 16:59:21 +0000

    bug fix

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from app import app, db
 from werkzeug.urls import url_parse
 
 @app.route('/')
-@app.route('/news')
+@app.route('/news', methods=['GET', 'POST'])
 def news():
     name = ''
     if current_user.is_authenticated:
