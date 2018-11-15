880dba5bff1539dd4fa423a02f32e9c832fc849f
U9601 <ctjones1965@gmail.com>
2018-11-15 20:41:02 +0000

    bug fix

--- app/routes.py
+++ app/routes.py
@@ -25,7 +25,6 @@ def news():
         count += 1
         teamname = team.text[3:]
         teams.append(teamname)
-        print(teams)
     newsform = NewsForm()
     if newsform.validate_on_submit():
         newspost = News(username=newsform.username.data, title=newsform.title.data, body=newsform.body.data)
