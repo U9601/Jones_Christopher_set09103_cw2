2f64e7490738798cceb75c85772e941d35dafc24
U9601 <ctjones1965@gmail.com>
2018-11-15 15:04:50 +0000

    bug fix

--- app/routes.py
+++ app/routes.py
@@ -20,7 +20,7 @@ def news():
     home = get_parsed_page("http://hltv.org/")
     count = 0
     teams = []
-    news = = NewsForm()
+    news = NewsForm()
     for team in home.find_all("div", {"class": ["col-box rank"], }):
         count += 1
         teamname = team.text[3:]
