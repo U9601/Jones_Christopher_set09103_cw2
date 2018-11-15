ef68c17692a9f7cdbfe5263d51b636e58bfa9e16
U9601 <ctjones1965@gmail.com>
2018-11-15 19:04:48 +0000

    bug fix

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,7 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -31,4 +31,4 @@ class NewsForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     title = StringField('Title', validators=[DataRequired()])
     body = StringField('Body', validators=[DataRequired()])
-    submit = SubmitField('News')
+    submit = SubmitField('Add News')
