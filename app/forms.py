d904e6e05bb9a9a90893585de5976278b2b7c88a
U9601 <ctjones1965@gmail.com>
2018-11-16 18:08:54 +0000

    adding event handlers

--- app/forms.py
+++ app/forms.py
@@ -18,9 +18,10 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Register')
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different username.')
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
@@ -37,3 +38,7 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Edit')
+
+    def __init__(self, orginal_user, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.orginal_username = orginal_username
