e76675dea410708d68792a48d6c9281a838cfc1d
U9601 <ctjones1965@gmail.com>
2018-11-16 17:58:33 +0000

    adding a email server

--- config.py
+++ config.py
@@ -6,3 +6,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('smtp.gmail.com')
+    MAIL_PORT = int(os.environ.get('587') or 25)
+    MAIL_USE_TLS = os.environ.get('1') is not None
+    MAIL_USERNAME = os.environ.get('ctjones1965@gmai.com')
+    MAIL_PASSWORD = os.environ.get('Lantaui1998')
+    ADMINS = ['ctjones19665@gmail.com']
