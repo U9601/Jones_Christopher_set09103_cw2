8c312e82463df23d72ea182f5f06642229b064ba
U9601 <ctjones1965@gmail.com>
2018-11-17 01:47:44 +0000

    bug fix

--- app/routes.py
+++ app/routes.py
@@ -147,7 +147,7 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form = form)
 
-@app.route('/reset_password', methods=['GET', 'POST'])
+@app.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
     if current_user.is_annoymous:
         return redirect(url_for('news'))
