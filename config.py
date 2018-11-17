8d80c73d4280ddb0c5a7f6bb1e6a74dc74e08d4d
U9601 <ctjones1965@gmail.com>
2018-11-17 02:05:27 +0000

    bug fix

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get(smtp.googlemail.com)
+    MAIL_SERVER = os.environ.get('smtp.googlemail.com')
     MAIL_PORT = int(os.environ.get(587) or 25)
     MAIL_USE_TLS = os.environ.get('1') is not None
     MAIL_USERNAME = os.environ.get('testserveremail123@gmail.com')
