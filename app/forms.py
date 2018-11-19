f38edb36e08f33cd73556f84cf77536103f89e70
U9601 <ctjones1965@gmail.com>
2018-11-19 12:34:53 +0000

    bug fix

--- app/forms.py
+++ app/forms.py
@@ -17,6 +17,10 @@ class RegistrationForm(FlaskForm):
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
     def validate_username(self, username):
         if username.data != self.original_username:
             user = User.query.filter_by(username=username.data).first()
