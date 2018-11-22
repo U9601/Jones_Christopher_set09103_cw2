4e60848cf75c7d748bbe8c4591b4ba66d4877a7d
U9601 <ctjones1965@gmail.com>
2018-11-22 12:09:09 +0000

    bug fix

--- app/routes.py
+++ app/routes.py
@@ -957,21 +957,6 @@ def EURONICSvsTricked():
         output.append(x)
     return render_template('matchdetails.html', form=form, name = name, output=output)
 
-@app.route('/resultsdetails/euronicsvstricked', methods=['GET', 'POST'])
-def EURONICSvsTricked():
-    name = 'euronicsvstricked'
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-    output = []
-    for x in resultslist17th:
-        output.append(x)
-    return render_template('matchdetails.html', form=form, name = name, output=output)
-
 @app.route('/resultsdetails/eunitedvsluminosity', methods=['GET', 'POST'])
 def eUnitedvsLuminosity():
     name = 'eunitedvsluminosity'
