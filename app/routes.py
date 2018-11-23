af2ee78a0e935197d942b4b389b398ee81ef9847
U9601 <ctjones1965@gmail.com>
2018-11-23 13:52:17 +0000

    bug fix

--- app/routes.py
+++ app/routes.py
@@ -154,12 +154,13 @@ def forum():
 @app.route('/comments/<post_id>' , methods=['GET', 'POST'])
 @login_required
 def comments(post_id):
+    post = Post.query.get(post_id)
     commentform = CommentForm()
+    comment = Comment(body=comment.comment.data, author=current_user, post_id=post.id)
     if commentform.validate_on_submit():
         comment = Comment(body=comment.comment.data, author=current_user, post_id=post.id)
         db.session.add(comment)
         db.session.commit()
-    post = Post.query.get(post_id)
     return render_template('comments.html', commentform=commentform, post_id=post_id, post=post)
 
 
