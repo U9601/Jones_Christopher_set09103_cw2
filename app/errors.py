2e5ff3fba6e4eab403466104ca4daeadae698ae5
U9601 <ctjones1965@gmail.com>
2018-11-23 14:47:17 +0000

    adding errors

--- app/errors.py
+++ app/errors.py
@@ -1,11 +1,26 @@
 from flask import render_template
 from app import app, db
+from app.forms import CommentForm
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+    return render_template('404.html', form=form), 404
 
 @app.errorhandler(500)
 def internal_error(error):
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('500.html', form=form), 500
