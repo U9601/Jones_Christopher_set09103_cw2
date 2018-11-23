a710ad13f765e37fafd199c0eb2b0e9bf44eb42e
U9601 <ctjones1965@gmail.com>
2018-11-23 18:33:31 +0000

    bug fix

--- app/models.py
+++ app/models.py
@@ -7,6 +7,11 @@ from time import time
 import jwt
 from app import app
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -72,11 +77,6 @@ class Comment(db.Model):
     body = db.Column(db.String(10000))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
-
-followers = db.Table('followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.integer, db.ForeignKey('user.id'))
-)
 
 @login.user_loader
 def load_user(id):
