98ac7f15cee4201f9324b87b0b8e8f6a3ce04b37
U9601 <ctjones1965@gmail.com>
2018-11-24 01:06:47 +0000

    bug fix

--- app/forms.py
+++ app/forms.py
@@ -26,7 +26,7 @@ class RegistrationForm(FlaskForm):
 class NewsForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     title = StringField('Title', validators=[DataRequired()])
-    body = TextAreaField('Body', validators=[DataRequired()])
+    body = StringField('Body', validators=[DataRequired()])
     submit = SubmitField('Add News')
 
 class EditProfileForm(FlaskForm):
