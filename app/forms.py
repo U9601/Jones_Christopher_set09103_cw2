e4e0a20272e93440c69e9f62416b7feef54901c3
U9601 <ctjones1965@gmail.com>
2018-11-24 13:04:51 +0000

    bug fix

--- app/forms.py
+++ app/forms.py
@@ -39,7 +39,7 @@ class EditProfileForm(FlaskForm):
 
     def __init__(self, orginal_user, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args, **kwargs)
-        self.orginal_username = orginal_username
+        self.original_username = orginal_username
 
     def validate_username(self, username):
         if username.data != self.original_username:
