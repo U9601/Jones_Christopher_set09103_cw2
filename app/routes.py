03c8f914c01f208a78d2996d72c41f5c7f3044a5
U9601 <ctjones1965@gmail.com>
2018-11-24 13:15:05 +0000

    adding login to user

--- app/routes.py
+++ app/routes.py
@@ -238,6 +238,13 @@ def register():
 @app.route('/user/<username>')
 @login_required
 def user(username):
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('news'))
+        login_user(user, remember=form.remember_me.data)
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
@@ -245,7 +252,7 @@ def user(username):
         if posts.has_next else None
     prev_url = url_for('user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('user.html', user = user, posts = posts.items, next_url = next_url, prev_url = prev_url)
+    return render_template('user.html', user = user, posts = posts.items, next_url = next_url, prev_url = prev_url, form=form)
 
 @app.route('/follow/<username>')
 @login_required
