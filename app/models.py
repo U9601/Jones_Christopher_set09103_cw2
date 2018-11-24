8699a2907fd1837b5c06298d9aa0247d6f23765c
U9601 <ctjones1965@gmail.com>
2018-11-24 04:36:41 +0000

    spelling mistake

--- app/models.py
+++ app/models.py
@@ -99,7 +99,7 @@ class User(UserMixin, db.Model):
             PostLike.user_id == self.id,
             PostLike.post_id == post.id).count() > 0
 
-    def new_message(self):
+    def new_messages(self):
         last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
         return Message.query.filter_by(recipient=self).filter(Message.timestamp > last_read_time).count()
 
