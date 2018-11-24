bc6f87d6c9434e56e32d5ffe226b93d7448816b8
U9601 <ctjones1965@gmail.com>
2018-11-24 03:52:30 +0000

    adding private messages

--- app/models.py
+++ app/models.py
@@ -35,6 +35,9 @@ class User(UserMixin, db.Model):
     liked = db.relationship(
         'PostLike', foreign_keys='PostLike.user_id',
         backref='user', lazy='dynamic')
+    message_sent = db.relationship('Message', foreign_keys='Message.sender_id', backref='author', lazy='dynamic')
+    messages_received = db.relationship('Message', foreign_keys='Message.recipient_id', backref='recipent', lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -97,6 +100,12 @@ class User(UserMixin, db.Model):
             PostLike.user_id == self.id,
             PostLike.post_id == post.id).count() > 0
 
+    def new_message(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(Message.timestamp > last_read_time).count()
+
+
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -133,6 +142,16 @@ class PostLike(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
 
 
 @login.user_loader
